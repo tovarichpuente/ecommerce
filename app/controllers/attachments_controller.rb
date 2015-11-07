cb95b3c7f3e408e51bbc4c33d40b7625d6f6f589
javiierriiveros <javiiriveros@hotmail.com>
2015-11-07 18:57:19 -0500

    Cambiar ruta de productos

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,25 +1,44 @@
 class AttachmentsController < ApplicationController
     before_action :authenticate_user!
-    before_action :authenticate_owner!
+    before_action :set_attachment, only: [:destroy,:show]
+    before_action :set_product, only: [:destroy]
+    before_action :authenticate_owner!, except: :show
 
     def new
+    end
+
+    def show
+        send_file @attachment.archivo.path
     end
 
     def create
         @attachment = Attachment.new(attachment_params)
         if @attachment.save
-            redirect_to @attachment.product, notice: "Se guardo el archivo adjunto" 
+            redirect_to @attachment.product, notice: "Se guardo el archivo adjunto"
         else
-            redirect_to @attachment.product, notice: "No se pudo guardar el archivo adjunto" 
+            redirect_to @attachment.product, notice: "No se pudo guardar el archivo adjunto"
         end
     end
 
     def destroy
+        @attachment.destroy
+        redirect_to @product
     end
 
     private
+    def set_attachment
+        @attachment = Attachment.find(params[:id])
+    end
+
+    def set_product
+        @product = @attachment.product
+    end
+
     def authenticate_owner!
-        @product = Product.find(params[:attachment][:product_id])
+        if params.has_key? :attachment
+            @product = Product.find(params[:attachment][:product_id])
+        end
+
         if @product.nil? || @product.user != current_user
             redirect_to root_path, notice: "No puedes editar este producto"
             return
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-	resources :attachments, only: [:new,:create,:destroy]
+	resources :attachments, only: [:new,:create,:destroy,:show]
     resources :products
 	devise_for :users
 
